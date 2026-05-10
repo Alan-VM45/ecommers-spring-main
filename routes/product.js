bef8a9eee0c514fec073221d16d82abeb9b935a1
Alan-VM45 <113520689+Alan-VM45@users.noreply.github.com>
2026-05-09 21:32:59 -0300

    nose

--- routes/product.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/productController');
-
-// GET /product/:id - Página de producto
-router.get('/:id', productController.getProductDetail);
-
-module.exports = router;
